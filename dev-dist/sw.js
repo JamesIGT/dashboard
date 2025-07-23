9bd900e0694fad637eb24b01934e761e49ff2212
jamesigt <jargtera@espol.edu.ec>
2025-07-23 13:35:42 -0500

    Dashboard

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "21199adf659a9b979572e74b6e36502f"
   }, {
     "url": "index.html",
-    "revision": "0.fbntnr5ml"
+    "revision": "0.7bttodceb5"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
